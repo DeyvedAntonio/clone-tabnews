3ced42a454099552c4f32c215539fcbccd0bf267
Deyved Antonio <deyved.antonio@gmail.com>
2023-08-25 23:09:51 +0000

    adicionar prettier `package.json`.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Você é uma pessoa incrível! Eu te amo muito.</h1>
+function Home() {
+  return <h1>Você é uma pessoa incrível! Eu te amo muito.</h1>;
 }
 
 export default Home;
